235d01a37e1770c2c53b60b82155a17ca0364d98
Dan Jakob Ofer <dan@ofer.to>
2025-12-26 09:03:42 -0500

    chore(init): first commit

---
+++ WindowlessScreenShotApp/WindowlessScreenShotAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  WindowlessScreenShotAppApp.swift
+//  WindowlessScreenShotApp
+//
+//  Created by Dan Ofer on 2025-12-26.
+//
+
+import SwiftUI
+
+@main
+struct WindowlessScreenShotAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ WindowlessScreenShotAppTests/WindowlessScreenShotAppTests.swift
@@ -0,0 +1,17 @@
+//
+//  WindowlessScreenShotAppTests.swift
+//  WindowlessScreenShotAppTests
+//
+//  Created by Dan Ofer on 2025-12-26.
+//
+
+import Testing
+@testable import WindowlessScreenShotApp
+
+struct WindowlessScreenShotAppTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
